332b2adf5a66dc3729a43d232973f88fc862d033
future28 <porky.dxp@gmail.com>
2015-01-03 10:00:29 +1100

    Simple backlight program

--- simple_lcd.ino
+++ simple_lcd.ino
@@ -4,10 +4,15 @@ LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 void setup(){
   lcd.begin(16, 2);
-  lcd.print("Testing, testing, 123");
+  lcd.print("Testing, testing");
 }
 
 void loop(){
   lcd.setCursor(0, 1);
   lcd.print(millis()/1000);
+
+  for(int i = 0; i < 16; i++){
+    lcd.scrollDisplayLeft();
+    delay(200);
+  }
 }
